6a2cde86d135975eb7d36a2b642a231e3a970edd
yulianchang <44862100+yulianchang@users.noreply.github.com>
2018-11-08 16:40:52 +0800

    Add files via upload

---
+++ testGit/testGit/testGit.h
@@ -0,0 +1,13 @@
+//
+//  testGit.h
+//  testGit
+//
+//  Created by yulc on 2018/11/8.
+//  Copyright © 2018年 huya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testGit : NSObject
+
+@end
---
+++ testGit/testGit/testGit.m
@@ -0,0 +1,13 @@
+//
+//  testGit.m
+//  testGit
+//
+//  Created by yulc on 2018/11/8.
+//  Copyright © 2018年 huya. All rights reserved.
+//
+
+#import "testGit.h"
+
+@implementation testGit
+
+@end
